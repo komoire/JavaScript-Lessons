f5c373f59eba48b46b284f351dbf677909ee56d2
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-10-10 18:09:24 +0300

    Delete number4a.js

--- number4a.js
+++
@@ -1,9 +0,0 @@
-let age = 18;
-
-if (age >= 18) {
-    console.log("You are eligible to work.");
-} else if (age < 18 && age > 0) { 
-    console.log("You are too young to work.");
-} else {
-    console.log("You are of retirement age.");
-}
