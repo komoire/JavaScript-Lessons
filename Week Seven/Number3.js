cb9555d580609e213f72b12d83f1101e7ba46eba
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-10-10 18:12:25 +0300

    Revision

---
+++ Week Seven/Number3.js
@@ -0,0 +1,11 @@
+// Declare a variable to store the temperature
+let temperature = 30;
+
+// Using an IF Statement
+if (temperature > 25) {
+    console.log("It's hot!");
+} else if (temperature > 15 && temperature <= 25) {
+    console.log("It's warm!");
+} else {
+    console.log("It's cold!");
+}
