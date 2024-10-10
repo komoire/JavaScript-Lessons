bdaf858a099b6f739ed41a815f472ba79d42f989
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-10-10 18:09:01 +0300

    Delete number2.js

--- number2.js
+++
@@ -1,9 +0,0 @@
-// Declaring a variable to store the student's name
-var studentName = "Komoire Ashiraf ";
-
-// Declaring a constant for the total number of students in the IT class
-const totalStudents = 30;
-
-// Displaying the student's name and the total number of students using console.log
-console.log("Student Name: " + studentName);
-console.log("Total number of students in IT class: " + totalStudents);
