a2ac69bb0279869aebeb08388351c86d8c7e3896
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-10-10 18:07:53 +0300

    Delete number4.js

--- number4.js
+++
@@ -1,9 +0,0 @@
-
-
-let age = 18;
-
-if (age >= 18) {
-    console.log("You are eligible to work.");
-} else {
-    console.log(age <= 0 ? "You are of retirement age." : "You are too young to work.");
-}
